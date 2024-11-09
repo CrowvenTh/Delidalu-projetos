d434a7855fc197184b65dd1e4d283c420b57470c
ThiagoSilva <thiagoantonio2108@gmail.com>
2024-11-09 17:34:00 -0300

    referência de autores e correções de layout

--- delidalu_php1.1/index.php
+++ delidalu_php1.1/index.php
@@ -63,12 +63,12 @@ session_start();
 
     <footer>
         <div class="footer-content">
-            <ul>
+            <ul class="autores">
                 <h3>Autores</h3>
-                <li>Thiago</li>
-                <li>Yago</li>
-                <li>Roney</li>
-                <li>Bryan</li>
+                <li><a href="https://github.com/CrowvenTh">Thiago</a></li>
+                <li><a href="https://github.com/akirar0n">Yago</a></li>
+                <li><a href="https://github.com/Yago-LDT">Roney</a></li>
+                <li><a href="https://github.com/Bryanjvo">Bryan</a></li>
             </ul>
             <ul>
                 <h3>Contato</h3>
@@ -85,7 +85,7 @@ session_start();
                 <h3>Redes Sociais</h3>
                 <li>Instagram</li>
                 <li>WhatsApp</li>
-                <li>Github</li>
+                <li><a href="https://github.com/CrowvenTh/Delidalu-projetos/tree/main">Github</a></li>
             </ul>
         </div>
     </footer>
