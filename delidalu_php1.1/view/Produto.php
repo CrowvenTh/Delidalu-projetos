4d739d6cb6b18774557725878f3c779f4120b622
ThiagoSilva <thiagoantonio2108@gmail.com>
2024-11-09 17:50:05 -0300

    correção de caminho para refeência de links internos

--- delidalu_php1.1/view/Produto.php
+++ delidalu_php1.1/view/Produto.php
@@ -17,10 +17,10 @@ session_start();
     <header>
         <nav>
             <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="controler/produtos.php">Produtos</a></li>
-                <li><a href="view/Login.php">Login</a></li>
-                <li><a href="view/CadCliente.php">Cadastro</a></li>
+                <li><a href="../index.php">Home</a></li>
+                <li><a href="Produto.php">Produtos</a></li>
+                <li><a href="Login.php">Login</a></li>
+                <li><a href="CadCliente.php">Cadastro</a></li>
             </ul>
         </nav>
     </header>
@@ -87,15 +87,14 @@ session_start();
             </section>
         </div>
     </main>
-
     <footer>
         <div class="footer-content">
-            <ul>
+            <ul class="autores">
                 <h3>Autores</h3>
-                <li>Thiago</li>
-                <li>Yago</li>
-                <li>Roney</li>
-                <li>Bryan</li>
+                <li> <img class="autoresImg" src="design_&_layout/logotipo/github-mark.png"> <a href="https://github.com/CrowvenTh">Thiago</a></li>
+                <li> <img class="autoresImg" src="design_&_layout/logotipo/github-mark.png"> <a href="https://github.com/akirar0n">Yago</a></li>
+                <li> <img class="autoresImg" src="design_&_layout/logotipo/github-mark.png"> <a href="https://github.com/Yago-LDT">Roney</a></li>
+                <li> <img class="autoresImg" src="design_&_layout/logotipo/github-mark.png"> <a href="https://github.com/Bryanjvo">Bryan</a></li>
             </ul>
             <ul>
                 <h3>Contato</h3>
@@ -112,7 +111,7 @@ session_start();
                 <h3>Redes Sociais</h3>
                 <li>Instagram</li>
                 <li>WhatsApp</li>
-                <li>Github</li>
+                <li><a href="https://github.com/CrowvenTh/Delidalu-projetos/tree/main">Github</a></li>
             </ul>
         </div>
     </footer>
